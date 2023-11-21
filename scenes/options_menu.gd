b9577e65e694b7187d0accf7754e4866a584cef7
Adriano Sudário <sudario.adriano@gmail.com>
2023-11-21 14:29:34 -0300

    Temporary options menu

---
+++ scenes/options_menu.gd
@@ -0,0 +1,66 @@
+class_name OptionsMenu
+extends Control
+
+@onready var options_container = $OptionsContainer
+
+var options = null
+var option_focused_index := 0
+
+func _ready():
+	var sound_container = $OptionsContainer/SoundContainer
+	sound_container.on_check.connect(
+		func(value):
+			Audio.is_on = value
+	)
+	sound_container.on_select.connect(
+		func(): sound_container.is_checked = !sound_container.is_checked
+	)
+	sound_container.is_checked = true
+	
+	var fullscreen_container = $OptionsContainer/FullscreenContainer
+	fullscreen_container.on_select.connect(
+		func(): fullscreen_container.is_checked = !fullscreen_container.is_checked
+	)
+	fullscreen_container.on_check.connect(
+		func(value):
+			if value:
+				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			else:
+				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	)
+	fullscreen_container.is_checked = false
+	
+	var back_container = $OptionsContainer/BackContainer
+	back_container.on_select.connect(
+		func():
+			SceneManager.load_string("res://scenes/main_menu.tscn")
+	)
+
+func change_next(next: int):
+	var previous_focused = options[option_focused_index]
+	previous_focused.is_focused = false
+	
+	option_focused_index += next
+	
+	if option_focused_index < 0:
+		option_focused_index = options.size() - 1
+	elif option_focused_index >= options.size():
+		option_focused_index = 0
+	
+	var focused_option = options[option_focused_index]
+	focused_option.is_focused = true
+
+func _process(_delta):
+	if options == null:
+		options = options_container.get_children()
+		option_focused_index = options.size() - 1
+		change_next(1)
+	
+	if Input.is_action_just_pressed("ui_down"):
+		change_next(1)
+	
+	if Input.is_action_just_pressed("ui_up"):
+		change_next(-1)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		options[option_focused_index].select()
---
+++ scripts/option_container.gd
@@ -0,0 +1,38 @@
+class_name OptionContainer
+extends HBoxContainer
+
+@onready var label:Label = $Label
+@onready var wavy_canvas = preload("res://shaders/wavy_canvas.gdshader")
+
+@export var is_focused: bool:
+	set(value):
+		is_focused = value
+		
+		if is_focused:
+			focus()
+		else:
+			unfocus()
+
+signal on_select
+
+const animation_duration := 0.25
+const unfocused_alpha := 0.5
+
+func _ready():
+	label.modulate.a = unfocused_alpha
+
+func focus():
+	label.material = ShaderMaterial.new()
+	label.material.shader = wavy_canvas
+	var t = create_tween().set_trans(Tween.TRANS_CUBIC)
+	t.tween_property(label, "modulate:a", 1.0, animation_duration)
+	return t
+
+func unfocus():
+	label.material = null
+	var t = create_tween().set_trans(Tween.TRANS_CUBIC)
+	t.tween_property(label, "modulate:a", unfocused_alpha, animation_duration)
+	return t
+
+func select():
+	on_select.emit()
