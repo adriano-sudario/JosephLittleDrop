a5840393479742c2fb2707bbcdcbc3cfd1e72fa8
Adriano Sudário <sudario.adriano@gmail.com>
2023-11-22 19:25:37 -0300

    Fixed crash after passing the last level

--- scripts/managers/level_manager.gd
+++ scripts/managers/level_manager.gd
@@ -3,6 +3,9 @@ class_name LevelManager
 static var current_level_index = 0
 
 static func get_current_level() -> Level:
+	if current_level_index >= LevelData.levels.size():
+		current_level_index = LevelData.levels.size() - 1
+	
 	return LevelData.levels[current_level_index]
 
 static func get_level(index:int) -> Level:
